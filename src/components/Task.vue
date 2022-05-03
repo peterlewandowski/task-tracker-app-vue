f318eb0a5c98d4a7936ec85c3f1108f82c8c82dc
Peter Lewandowski <mapiotr2010@gmail.com>
2022-05-02 21:35:01 -0400

    created Task component

---
+++ src/components/Task.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="task">
+        <h3>{{ task.text }}
+        <i class="fas fa-times"></i>
+        <!-- font-awesome imported to index.html; -->
+        </h3>
+        <p>{{ task.day }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Task',
+    props: {
+        task: Object // this component will take in an individual "task" object
+    }
+}
+</script>
+
+<style scope>
+.fas {
+    color: red;
+}
+
+.task {
+    background: #f4f4f4;
+    margin: 5px;
+    padding: 10px 20px;
+    cursor: pointer;
+}
+
+.task.reminder {
+    border-left: 5px solid green;
+}
+
+.task h3 {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+</style>
