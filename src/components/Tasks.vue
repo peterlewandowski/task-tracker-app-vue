0e7d933bdc5da6686a565b4516d4417fa07e0cf9
Peter Lewandowski <mapiotr2010@gmail.com>
2022-05-02 21:35:34 -0400

    registered Task component, added to template

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,15 +1,18 @@
 <template>
     <div :key="task.id" v-for="task in tasks">
-    <!-- like 2: v-for loops through each task in tasks -->
-    <h3>{{task.text}}</h3>
+        <!-- like 2: v-for loops through each task in tasks -->
+        <Task :task="task" />
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'Tasks',
-        props: {
-            tasks: Array
-        }
-    }
+import Task from "./Task.vue";
+
+export default {
+    name: "Tasks", // name of this component
+    props: { // this component "Tasks" will take in "tasks" Array as prop
+        tasks: Array
+    },
+    components: { Task } // need to register the Task component inside this "Tasks" component
+}
 </script>
