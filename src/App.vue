112a0429e1120ae28b8747b43fde7e194da482bb
Peter Lewandowski <mapiotr2010@gmail.com>
2022-05-04 18:15:59 -0400

    set up fetch requests to json-server backend via proxy

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,14 @@ export default {
       /* the opposite of the current task reminder. Else, if it doesn't match the id, just return the initial task  */
     },
     async fetchTasks() {
-      const res = await fetch('http://localhost:5000/tasks')
+      const res = await fetch('api/tasks')
+
+      const data = await res.json()
+
+      return data
+    },
+    async fetchTask(id) {
+      const res = await fetch(`api/tasks/${id}`)
 
       const data = await res.json()
 
