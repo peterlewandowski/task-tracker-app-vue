260ef5ffca8214b98b00e13c49f052bd13866c1b
Peter Lewandowski <mapiotr2010@gmail.com>
2022-05-05 22:14:59 -0400

    math.random not needed, json-server generates id for new task

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -37,7 +37,7 @@ export default {
             }
 
             const newTask = {
-                id: Math.floor(Math.random() * 10000),
+                // id: Math.floor(Math.random() * 10000), /* not needed, json-server generates id for new task */
                 text: this.text,
                 day: this.day,
                 reminder: this.reminder
